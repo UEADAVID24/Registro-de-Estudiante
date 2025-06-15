6e8a64bb0fd38289f8de1dd16c434c6c953cafcd
UEADAVID24 <jonasalvarado19987@gmail.com>
2025-06-15 17:08:03 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace RegistroEstudiante
+{
+    class Estudiante
+    {
+        public int Id { get; set; }
+        public string Nombres { get; set; }
+        public string Apellidos { get; set; }
+        public string Direccion { get; set; }
+        public string[] Telefonos { get; set; }
+
+        // Constructor que obliga a inicializar todas las propiedades
+        public Estudiante(int id, string nombres, string apellidos, string direccion, string[] telefonos)
+        {
+            Id = id;
+            Nombres = nombres;
+            Apellidos = apellidos;
+            Direccion = direccion;
+            Telefonos = telefonos;
+        }
+
+        public void MostrarDatos()
+        {
+            Console.WriteLine($"ID: {Id}");
+            Console.WriteLine($"Nombres: {Nombres}");
+            Console.WriteLine($"Apellidos: {Apellidos}");
+            Console.WriteLine($"Dirección: {Direccion}");
+            Console.WriteLine("Teléfonos:");
+            for (int i = 0; i < Telefonos.Length; i++)
+            {
+                Console.WriteLine($"Teléfono {i + 1}: {Telefonos[i]}");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var estudiante = new Estudiante(
+                1,
+                "David",
+                "Alvarado",
+                "Avenida 9 de Octubre",
+                new string[] { "0969920207", "0996424516", "0979923493" }
+            );
+
+            estudiante.MostrarDatos();
+
+            Console.ReadKey();
+        }
+    }
+}
